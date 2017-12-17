4bdb967148a01f66259da620054f7e9de8125ed7
badrabbit <badrabbit@debian>
2017-12-17 22:06:37 +0300

    concurrent benchmark for Withraw

--- fund_test.go
+++ fund_test.go
@@ -1,7 +1,51 @@
 package funding
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
+const WORKERS = 10
+
+func BenchmarkWithdrawals(b *testing.B) {
+	if b.N < WORKERS {
+		return
+	}
+
+	// Add as many dollars as we have interations this run
+	fund := NewFund(b.N)
+
+	// Casually assume b.N divides cleanly
+	dollarsPerFounder := b.N / WORKERS
+
+	// WaitGroup structs don't need to be initialized
+	// (their "zero value" is ready to use).
+	// So, we just declare one and then use it
+	var wg sync.WaitGroup
+
+	for i := 0; i < WORKERS; i++ {
+		// Let the waitgroup know we're adding a goroutine
+		wg.Add(1)
+
+		// Spawn off a founder worker, as a closure
+		go func() {
+			// Mark this worker done when the functions finishes
+			defer wg.Done()
+
+			for i := 0; i < dollarsPerFounder; i++ {
+				fund.Withdraw(1)
+			}
+		}() // Remember to call the closure!
+	}
+
+	// Wait for all the workrers to finish
+	wg.Wait()
+
+	if fund.Balance() != 0 {
+		b.Error("Balance wasn't zero:", fund.Balance())
+	}
+
+}
 func BenchmarkFund(b *testing.B) {
 	// Add as many dollars as we have iterations this run
 	fund := NewFund(b.N)
