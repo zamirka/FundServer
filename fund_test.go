0ca18ff6731cb27ab7ba8dd95863775f4bf17126
Замир Мусаев <zamir.ru@gmail.com>
2017-12-28 22:54:55 +0300

    Encapsulated version of benchmark

--- fund_test.go
+++ fund_test.go
@@ -49,6 +49,47 @@ func BenchmarkWithdrawals(b *testing.B) {
 	}
 
 }
+
+func BenchmarkWithdrawalsOneMoreTime(b *testing.B) {
+	if b.N < WORKERS {
+		return
+	}
+
+	// Casually assume b.N divides cleanly
+	dollarsPerFounder := b.N / WORKERS
+
+	server := NewFundServer(b.N)
+
+	// WaitGroup structs don't need to be initialized
+	// (their "zero value" is ready to use).
+	// So, we just declare one and then use it
+	var wg sync.WaitGroup
+
+	for i := 0; i < WORKERS; i++ {
+		// Let the waitgroup know we're adding a goroutine
+		wg.Add(1)
+
+		// Spawn off a founder worker, as a closure
+		go func() {
+			// Mark this worker done when the functions finishes
+			defer wg.Done()
+
+			for i := 0; i < dollarsPerFounder; i++ {
+				server.Withdraw(1)
+			}
+		}() // Remember to call the closure!
+	}
+
+	// Wait for all the workrers to finish
+	wg.Wait()
+
+	balance := server.Balance()
+
+	if balance != 0 {
+		b.Error("Balance wasn't zero:", balance)
+	}
+
+}
 func BenchmarkFund(b *testing.B) {
 	// Add as many dollars as we have iterations this run
 	fund := NewFund(b.N)
