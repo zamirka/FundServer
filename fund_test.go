8677d7fec1c5aaa4eaec5359f87dd710c0fb01c4
Замир Мусаев <zamir.ru@gmail.com>
2017-12-28 22:51:02 +0300

    fixed encapsulated benchmark. Reason: there were no consumers fo commands, just enqueue

--- fund_test.go
+++ fund_test.go
@@ -32,49 +32,6 @@ func BenchmarkWithdrawals(b *testing.B) {
 			defer wg.Done()
 
 			for i := 0; i < dollarsPerFounder; i++ {
-				server.Commands <- WithdrawCommand{Amount: 1}
-			}
-		}() // Remember to call the closure!
-	}
-
-	// Wait for all the workrers to finish
-	wg.Wait()
-
-	balanceResponceChan := make(chan int)
-	server.Commands <- BalanceCommand{Response: balanceResponceChan}
-	balance := <-balanceResponceChan
-
-	if balance != 0 {
-		b.Error("Balance wasn't zero:", balance)
-	}
-
-}
-
-func BenchmarkWithdrawalsOneMoreTime(b *testing.B) {
-	if b.N < WORKERS {
-		return
-	}
-
-	// Casually assume b.N divides cleanly
-	dollarsPerFounder := b.N / WORKERS
-
-	server := NewFundServer(b.N)
-
-	// WaitGroup structs don't need to be initialized
-	// (their "zero value" is ready to use).
-	// So, we just declare one and then use it
-	var wg sync.WaitGroup
-
-	for i := 0; i < WORKERS; i++ {
-		// Let the waitgroup know we're adding a goroutine
-		wg.Add(1)
-
-		// Spawn off a founder worker, as a closure
-		go func() {
-			// Mark this worker done when the functions finishes
-			defer wg.Done()
-
-			for i := 0; i < dollarsPerFounder; i++ {
 				server.Withdraw(1)
 			}
 		}() // Remember to call the closure!
@@ -90,6 +47,7 @@ func BenchmarkWithdrawalsOneMoreTime(b *testing.B) {
 	}
 
 }
+
 func BenchmarkFund(b *testing.B) {
 	// Add as many dollars as we have iterations this run
 	fund := NewFund(b.N)
